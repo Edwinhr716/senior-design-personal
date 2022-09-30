f29da4a6d260d68014cbab5891228bfd390a22bb
Edwin Hernandez <Edwinhr716@utexas.edu>
2022-09-30 16:45:21 -0500

    finished the script, includes the zip files of the data that was gathered

--- scraping_script.py
+++ scraping_script.py
@@ -1,12 +1,11 @@
+import time
 import requests
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 import nltk
-nltk.download('punkt')
 from nltk import sent_tokenize, word_tokenize
 
 
@@ -26,7 +25,7 @@ class ScrapeData:
         return pd.DataFrame(graduation_data)
 
     """
-    input is university name, and url
+    input is university name, and url to get enrollment data
     returns a Pandas df
 
     Since the dataset also contains average data of all universities,
@@ -51,44 +50,55 @@ class ScrapeData:
         all_paragraphs = soup.find_all("p")
         about_paragraph = all_paragraphs[2].text.strip()
         sentences_about_paragraph = sent_tokenize(about_paragraph)
-        words_in_first_sentence = words_in_first_sentence = word_tokenize(sentences_about_paragraph[0])
+        words_in_first_sentence = word_tokenize(sentences_about_paragraph[0])
+        #-2 because the last character is a period
         return words_in_first_sentence[len(words_in_first_sentence) - 2]
 
     """
-    TODO:(Figure out how to scrape all of the IDs for universities)
+    returns a set with all of the university ids. 
+    It is a set because all_university_data contains duplicates of the universities
     """
     def get_university_ids(self):
-        pass
+        ids = set()
+        response = requests.get("https://datausa.io/api/data?Geography=04000US00&measures=Completions&drilldowns=Sector,University")
+        all_universities_data = response.json()["data"]
+        for university_data in all_universities_data:
+            ids.add(university_data["ID University"])
+        return ids
 
     """
     Main logic of scraping and combining data
     Creates two csv files in same directory
     """
     def get_data(self):
-        university_ids = [221768, 211440, 228778]
+        university_ids = self.get_university_ids()
         combined_enrollment_df = None
         combined_graduation_df = None
         for id in university_ids:
+            time.sleep(0.25)
             graduation_data_df = self.get_graduation_data(f"https://preview.datausa.io/api/data?University={id}&measures=Graduation Rate,Number Of Finishers&drilldowns=Gender,IPEDS Race&Number Of Finishers>=5")
-            university_url = graduation_data_df.iloc[0]['Slug University']
-            state = self.get_university_state(university_url)
-            university_name = graduation_data_df.iloc[0]['University']
-            state_array = [state]*len(graduation_data_df)
-            graduation_data_df["Location"] = state_array
+            try:
+                university_url = graduation_data_df.iloc[0]['Slug University']
+                state = self.get_university_state(university_url)
+                university_name = graduation_data_df.iloc[0]['University']
+                state_array = [state]*len(graduation_data_df)
+                graduation_data_df["Location"] = state_array
 
-            enrollment_data_df = self.get_enrollment_data(university_name, f"https://preview.datausa.io/api/data?University={id},{id}:parents&measures=Enrollment&drilldowns=IPEDS%20Race")
-            state_array = [state]*len(enrollment_data_df)
-            enrollment_data_df["Location"] = state_array
+                enrollment_data_df = self.get_enrollment_data(university_name, f"https://preview.datausa.io/api/data?University={id},{id}:parents&measures=Enrollment&drilldowns=IPEDS%20Race")
+                state_array = [state]*len(enrollment_data_df)
+                enrollment_data_df["Location"] = state_array
 
-            if graduation_data_df is None:
-                combined_graduation_df = graduation_data_df
-            else:
-                combined_graduation_df = pd.concat([combined_graduation_df, graduation_data_df], axis=0)
-            
-            if combined_enrollment_df is None:
-                combined_enrollment_df = enrollment_data_df
-            else:
-                combined_enrollment_df = pd.concat([combined_enrollment_df, enrollment_data_df], axis=0)
+                if graduation_data_df is None:
+                    combined_graduation_df = graduation_data_df
+                else:
+                    combined_graduation_df = pd.concat([combined_graduation_df, graduation_data_df], axis=0)
+                
+                if combined_enrollment_df is None:
+                    combined_enrollment_df = enrollment_data_df
+                else:
+                    combined_enrollment_df = pd.concat([combined_enrollment_df, enrollment_data_df], axis=0)
+            except:
+                print("No Information Found")
         
         combined_enrollment_df.to_csv("enrollment_data.csv")
         combined_graduation_df.to_csv("graduation_data.csv")
@@ -97,7 +107,7 @@ class ScrapeData:
 
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Firefox(executable_path="geckodriver.exe")
     data_scraping = ScrapeData(driver)
     data_scraping.get_data()
 
